7c16d25fd7726c04cfbeb499563ec6fbdf5529e8
Rile-Cry <rileycriddle@yahoo.com>
2025-06-12 05:42:41 -0400

    Audio
    Imported audio from previous project and added them to the Audio System.

--- autoloads/audio/sound_loader.gd
+++ autoloads/audio/sound_loader.gd
@@ -13,7 +13,24 @@ func load_ost() -> void:
 	# Add OST loads here in format (int: [preload(song_path), end_of_loop (-1 for no looping)]
 	# Ex:
 	#		0: [preload("res://assets/audio/ost/theme.wav"), 356]
-	var song_list : Dictionary[int, Array] = {}
+	var song_list : Dictionary[int, Array] = {
+		0: [preload("res://assets/audio/music/Evelyn Photo Intro.wav"), -1],
+		1: [preload("res://assets/audio/music/Evelyn Photo Loop.wav"), 0],
+		2: [preload("res://assets/audio/music/Finale Intro.wav"), -1],
+		3: [preload("res://assets/audio/music/Finale Loop.wav"), 0],
+		4: [preload("res://assets/audio/music/Interrogation Room Intro.wav"), -1],
+		5: [preload("res://assets/audio/music/Interrogation Room Loop.wav"), 0],
+		6: [preload("res://assets/audio/music/Marina Photo Intro.wav"), -1],
+		7: [preload("res://assets/audio/music/Marina Photo Loop.wav"), 0],
+		8: [preload("res://assets/audio/music/Marina's Apartment Intro.wav"), -1],
+		9: [preload("res://assets/audio/music/Marina's Apartment Loop.wav"), 0],
+		10: [preload("res://assets/audio/music/Shade's Office Intro.wav"), -1],
+		11: [preload("res://assets/audio/music/Shade's Office Loop.wav"), 0],
+		12: [preload("res://assets/audio/music/Title Screen Intro.wav"), -1],
+		13: [preload("res://assets/audio/music/Title Screen Loop.wav"), 0],
+		14: [preload("res://assets/audio/music/Victor's Office Intro.wav"),  -1],
+		15: [preload("res://assets/audio/music/Victor's Office Loop.wav"), 0]
+	}
 	
 	for song in song_list.values():
 		song.get(0).resource_name = grab_name(song.get(0).resource_path)
@@ -22,7 +39,14 @@ func load_ost() -> void:
 ## Loads SFX into the SFX BUS of SoundManager
 func load_sfx() -> void:
 	# Add SFX loads here in format: preload(sfx_path)
-	var sfx_list : Array[AudioStream] = []
+	var sfx_list : Array[AudioStream] = [
+		preload("res://assets/audio/sfx/Arrest.wav"),
+		preload("res://assets/audio/sfx/Door Knock.wav"),
+		preload("res://assets/audio/sfx/Door Open.wav"),
+		preload("res://assets/audio/sfx/Exclaim.wav"),
+		preload("res://assets/audio/sfx/Screen Shake.wav"),
+		preload("res://assets/audio/sfx/Screen Shake Aggressive.wav"),
+	]
 	
 	for sfx in sfx_list:
 		sfx.resource_name = grab_name(sfx.resource_path)
@@ -31,7 +55,22 @@ func load_sfx() -> void:
 ## Loads UI SFX into the UI BUS of SoundManager
 func load_ui() -> void:
 	# Add UI loads here in format: preload(ui_sfx_path)
-	var ui_list : Array[AudioStream] = []
+	var ui_list : Array[AudioStream] = [
+		preload("res://assets/audio/sfx/Book Close.wav"),
+		preload("res://assets/audio/sfx/Book Hover.wav"),
+		preload("res://assets/audio/sfx/Book Open.wav"),
+		preload("res://assets/audio/sfx/Book Turn.wav"),
+		preload("res://assets/audio/sfx/Mouse Click Fail.wav"),
+		preload("res://assets/audio/sfx/Mouse Click.wav"),
+		preload("res://assets/audio/sfx/Object Hover 1.wav"),
+		preload("res://assets/audio/sfx/Object Hover 2.wav"),
+		preload("res://assets/audio/sfx/Object Hover 3.wav"),
+		preload("res://assets/audio/sfx/Object Hover 4.wav"),
+		preload("res://assets/audio/sfx/Object Select.wav"),
+		preload("res://assets/audio/sfx/Popup.wav"),
+		preload("res://assets/audio/sfx/Sticky Note Hover.wav"),
+		preload("res://assets/audio/sfx/UI Open Close.wav")
+	]
 	
 	for ui in ui_list:
 		ui.resource_name = grab_name(ui.resource_path)
@@ -40,7 +79,12 @@ func load_ui() -> void:
 ## Loads Ambient SFX into the Ambient BUS of SoundManager
 func load_ambient() -> void:
 	# Add Ambient loads here in format: preload(ambient_sfx_path)
-	var ambient_list : Array[AudioStream] = []
+	var ambient_list : Array[AudioStream] = [
+		preload("res://assets/audio/ambient/Interrogation Room.wav"),
+		preload("res://assets/audio/ambient/OS Boot.wav"),
+		preload("res://assets/audio/ambient/Photo Jump.wav"),
+		preload("res://assets/audio/ambient/Shade's Office.wav")
+	]
 	
 	for ambient in ambient_list:
 		ambient.resource_name = grab_name(ambient.resource_path)
